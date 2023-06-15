d6a53cad9694a68f33023a2aea474364e0c692ce
Quethzel Diaz <quethzel@gmail.com>
2023-06-14 22:30:08 -0600

    fix select in expense form. minor refactor

--- src/app/components/expense-form/expense-form.component.ts
+++ src/app/components/expense-form/expense-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { IUserCity } from 'src/app/models/interfaces/ICity';
 import { IExpenses } from 'src/app/models/interfaces/IExpenses';
 import { ExpensesService } from 'src/app/services/expenses.service';
 
@@ -10,45 +11,78 @@ import { ExpensesService } from 'src/app/services/expenses.service';
   styleUrls: ['./expense-form.component.scss']
 })
 export class ExpenseFormComponent implements OnInit {
-  expenseForm!: FormGroup;
   expense!: IExpenses;
+  expenseForm!: FormGroup;
+  cities: IUserCity[] = [];
+  cityCode = '---';
+  maxDate: Date;
 
-  myForm?: UntypedFormGroup;
-  
   constructor(
+    private fb: FormBuilder,
     public bsModalRef: BsModalRef,
-    private formBuilder: FormBuilder,
     private expService: ExpensesService,
-    private fb: UntypedFormBuilder) { }
+    ) {
+      
+      this.maxDate = new Date();
+
+    }
 
   ngOnInit(): void {
-    this.expenseForm = this.formBuilder.group({
-      city: ['', Validators.required],
-      cityCode: [''],
+    this.expenseForm = this.fb.group({
+      city: new FormControl(),
+      // city: this.fb.group({
+      //   code: [''],
+      //   name: [''],
+      //   default: [false]
+      // }),
       item: ['', Validators.required],
       category: ['', Validators.required],
       subcategory: ['', Validators.required],
       cost: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
-      date: ['', Validators.required]
+      date: [new Date(), Validators.required]
     });
 
-    this.myForm = this.fb.group({
-      date: null,
-      range: null
-    });
+    this.cities = [
+      { code: 'MTY', name: 'Monterrey', default: true },
+      { code: 'CDMX', name: 'CDMX', default: false },
+      { code: 'XAL', name: 'Xalapa', default: false },
+      { code: 'OAX', name: 'Oaxaca', default: false },
+    ];
 
-    // this.expenseForm.controls['city'].setValue("MTY");
+    this.setDefaultCity();
   }
 
-  chageCity($event: Event) {
-    this.expenseForm.patchValue({
-      cityCode: ($event.target as HTMLInputElement).value
-    });
+  onChangeCity($event: any) {
+    const cc = $event.target.selectedOptions[0]?.dataset?.value;
+    this.cityCode = cc ? cc : '---';
+  }
+
+  setPreviousDate() {
+    let prevDate = new Date();
+    const selectedDate = this.expenseForm.controls['date'].value;
+
+    if (selectedDate instanceof Date) {
+      prevDate.setDate(selectedDate.getDate() - 1);
+      this.expenseForm.controls['date'].setValue(prevDate);
+    } else {
+      alert('Selected Date is invalid!');
+    }
+  }
+
+  setDefaultCity() {
+    const defaultCity = this.cities.find(c => c.default == true);
+    if (defaultCity) {
+      this.expenseForm.controls['city'].setValue(defaultCity, {onlySelf: true});
+      this.cityCode = defaultCity.code;
+    } else {
+      this.expenseForm.controls['city'].setValue(this.cities[0]);
+      this.cityCode = this.cities[0].code;
+    }
   }
 
   onSubmit(): void {
-    this.expense = this.expenseForm.getRawValue();
-    
+    this.expense = this.mapToExpense(this.expenseForm.getRawValue());
+  
     this.expService.saveExpense(this.expense).subscribe({
       next: (expense) => {
         this.bsModalRef.hide();
@@ -58,6 +92,18 @@ export class ExpenseFormComponent implements OnInit {
         console.error(e);
       }
     });
+  }
+
+  private mapToExpense(data: any) {
+    return {
+      city: data.city.name,
+      cityCode: data.city.code,
+      item: data.item,
+      category: data.category,
+      subcategory: data.subcategory,
+      cost: data.cost,
+      date: data.date
+    } as IExpenses;
   }
 
 
---
+++ src/app/models/interfaces/ICity.ts
@@ -0,0 +1,6 @@
+export interface IUserCity {
+    code: string;
+    name: string;
+    default: boolean;
+}
+
