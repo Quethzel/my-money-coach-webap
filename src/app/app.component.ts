fd1cdb8d4ac978858cdfe9fee40ab46ed1208951
Quethzel <quethzel@gmail.com>
2023-09-27 23:05:49 -0600

    upgrade version related to annual expenses feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { UserService } from './services/user.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  version = 'v1.02.09';
+  version = 'v1.02.10';
 
   constructor(private userService: UserService) {
     this.userService.loadCities();
